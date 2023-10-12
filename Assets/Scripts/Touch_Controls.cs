595361a0e6294b959ac86a76d95becf334c8cd8f
Kavyansh Barjattya <117171893+kavyanshbarjattya@users.noreply.github.com>
2023-10-12 23:05:36 +0530

    fixing bugs ft. anmol bhai

--- Assets/Scripts/Touch_Controls.cs
+++ Assets/Scripts/Touch_Controls.cs
@@ -2,16 +2,16 @@ using UnityEngine;
 
 public class Touch_Controls : MonoBehaviour
 {
-    [SerializeField] float _UpwardForce, _range, _dashTimer , _doubleSwipeTimer;
+    [SerializeField] float _UpwardForce, _dashTimer , _doubleSwipeTimer;
     [SerializeField] LayerMask _whatIsGrounded;
 
     bool _isgrounded ;
     int _doubleJumpCounter , _doubleSwipeDownCounter , dashCounter;
+    float _range;
     float _jumpForce, _nextDashTimer , _nextDoubleSwipeTimer;
     Rigidbody rb;
     PlayerMovement playerMove;
-    Vector3 startTouchPos;
-    Vector3 endTouchPos;
+    Vector2 startTouchPos , endTouchPos;
     RaycastHit hit;
     private void Awake()
     {
@@ -21,36 +21,39 @@ public class Touch_Controls : MonoBehaviour
     private void Start()
     {
         _jumpForce = _UpwardForce;
+        _range = transform.localScale.y;
     }
 
     private void Update()
     {
         Raycast();
         Touch();
-        if(_isgrounded)
+
+        if (_isgrounded)
         {
             _doubleJumpCounter = 0;
         }
 
-        if(Time.time > _nextDashTimer)
+        if (Time.time > _nextDashTimer)
         {
             dashCounter = 0;
             _nextDashTimer = Time.time + _dashTimer;
         }
-        if(dashCounter == 2)
+        if (dashCounter == 2)
         {
             Debug.Log("Dash....");
         }
-/*
-        if(_doubleSwipeDownCounter == 2)
+
+        if (_doubleSwipeDownCounter == 2)
         {
             Debug.Log("Player is underGround");
+            _doubleSwipeDownCounter = 0;
         }
-        if(Time.time > _nextDoubleSwipeTimer)
+        if (Time.time > _nextDoubleSwipeTimer)
         {
             _doubleSwipeDownCounter = 0;
             _nextDoubleSwipeTimer = Time.time + _doubleSwipeTimer;
-        }*/
+        }
     }
     void Touch()
     {
@@ -63,39 +66,56 @@ public class Touch_Controls : MonoBehaviour
         {
             endTouchPos = Input.GetTouch(0).position;
         }
-
-        if(endTouchPos.x > startTouchPos.x)
+        if (Mathf.Abs(endTouchPos.x - startTouchPos.x) > Mathf.Abs(endTouchPos.y - startTouchPos.y))
         {
-            startTouchPos = Vector3.zero;
-            endTouchPos = Vector3.zero;
-            if (dashCounter <= 1)
+            if ((endTouchPos.x > startTouchPos.x) && startTouchPos.x != 0 && endTouchPos.x != 0)
             {
-                dashCounter++;
+                if (dashCounter <= 1)
+                {
+                    dashCounter++;
+                }
+                startTouchPos = Vector2.zero;
+                endTouchPos = Vector2.zero;
+            }
+        }
+        else if(Mathf.Abs(endTouchPos.x - startTouchPos.x) < Mathf.Abs(endTouchPos.y - startTouchPos.y))
+        {
+
+            if ((endTouchPos.y > startTouchPos.y) && startTouchPos.y != 0 && endTouchPos.y != 0)
+            {
+                Debug.Log("Bs aise hi");
+                if (_doubleJumpCounter <= 0)
+                {
+                    _doubleJumpCounter++;
+                    rb.AddForce(Vector3.up * _jumpForce * 1000 * Time.deltaTime, ForceMode.Force);
+                }
+                startTouchPos = Vector2.zero;
+                endTouchPos = Vector2.zero;
+            }
+            else if ((endTouchPos.y < startTouchPos.y) && startTouchPos.y != 0 && endTouchPos.y != 0)
+            {
+                Debug.Log("Duck");
+                if(_doubleSwipeDownCounter <= 1)
+                {
+                    _doubleSwipeDownCounter++;
+                }
+                startTouchPos = Vector2.zero;
+                endTouchPos = Vector2.zero;
             }
         }
 
-        if (endTouchPos.y > startTouchPos.y)
-        {
-            startTouchPos = Vector3.zero;
-            endTouchPos = Vector3.zero;
-            if (_doubleJumpCounter <= 0)
-            {
-                _doubleJumpCounter++;
-                Debug.Log("Hello");
-                rb.AddForce(Vector3.up * _jumpForce * 1000 * Time.deltaTime, ForceMode.Force);
-            }
-        }
     }
 
     void Raycast()
     {
-        if(Physics.Raycast(transform.position , transform.TransformDirection(Vector3.down) , out hit, _range, _whatIsGrounded))
+        if(Physics.Raycast(transform.position , Vector3.down , out hit, _range, _whatIsGrounded))
         {
             _isgrounded = true;
         }
         else
         {
             _isgrounded = false;
+
         }
     }
 
